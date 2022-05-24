3503175de43b5e5dbc46a1b2a2b0d027481b487b
VinokurovDmitriy <78742044+VinokurovDmitriy@users.noreply.github.com>
2022-05-24 23:18:05 +0300

    Update Program.cs
    add new solution

--- c#_dz/dz9/task64/Program.cs
+++ c#_dz/dz9/task64/Program.cs
@@ -4,14 +4,7 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int NumSum( int num )
 {
-    if(num < 10) 
-    {
-        return num;
-    }
-    else 
-    {
-       return ((num % 10) + NumSum( num / 10 ));
-    }
+    return (num < 10) ? num : num % 10 + NumSum num / 10 ;
 }
 
 Console.WriteLine($"Сумма цифр числа {n} = {NumSum(n)}");
