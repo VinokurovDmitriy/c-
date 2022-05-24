abc47ef1c4040146d08edff86eca3e99bfbf02fb
VinokurovDmitriy <78742044+VinokurovDmitriy@users.noreply.github.com>
2022-05-24 23:36:52 +0300

    Update Program.cs

--- c#_dz/dz9/task68/Program.cs
+++ c#_dz/dz9/task68/Program.cs
@@ -1,15 +1,21 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-Console.Write("Введите число m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+bool correctEnter = false;
+int m = 0;
+int n = 0;
+while(!correctEnter)
+{
+    Console.Write("Введите число m: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число n: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    correctEnter = m > 0 && n > 0;
+}
 int FunctionAckermann(int n, int m)
             {
                 if (n == 0) return m + 1;
                 if (n != 0 && m == 0) return FunctionAckermann(n - 1, 1);
-                if (n > 0 && m > 0) return FunctionAckermann(n - 1, FunctionAckermann(n, m - 1));
-                return FunctionAckermann(n,m);
+                return FunctionAckermann(n - 1, FunctionAckermann(n, m - 1));
             }
 
 Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {FunctionAckermann(m, n)}");
