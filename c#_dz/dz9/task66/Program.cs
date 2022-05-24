427b03d882d73ff53d604cb1de200144101766d2
VinokurovDmitriy <78742044+VinokurovDmitriy@users.noreply.github.com>
2022-05-24 23:15:35 +0300

    Update Program.cs
    new solutiom

--- c#_dz/dz9/task66/Program.cs
+++ c#_dz/dz9/task66/Program.cs
@@ -1,15 +1,13 @@
-﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-
-int GetSum(int m, int n, int sum = 0)
+int GetSum(int m, int n)
 {
-    if (m <= n)
-        sum += m + GetSum(m + 1, n, sum);
-    return sum;
+   return m == n ? m : m + GetSum(m + 1, n);
 }
-Console.WriteLine($"Сумма всех чисел от {m} до  {n} = {GetSum(m, n)}");
+Console.WriteLine($"Сумма всех чисел от {m} до {n} = {GetSum(m, n)}");
+
